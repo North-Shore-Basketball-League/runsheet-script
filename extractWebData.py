8971305e73fed325e0243e2280430b1714462bcb
tobsterclark <me@tobyclark.dev>
2023-07-27 15:04:22 +1000

    Update extractWebData.py trying to fix chrome web driver

--- extractWebData.py
+++ extractWebData.py
@@ -13,7 +13,7 @@ class ExtractWebData:
         options.add_argument("--headless")
         options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
-        driver = Chrome(service=ChromeService(ChromeDriverManager(version='114.0.5735.90').install()), options=options, service_log_path="NUL")
+        webdriver.Chrome(ChromeDriverManager(version='114.0.5735.90').install(), options=options)
 
     def __del__(self):
         self.driver.quit()
